2210a21cd8e50b895823c04df07a4b20ab252ddd
Oever González <software@notengobattery.com>
2021-03-16 10:12:15 -0600

    Update and setup the template for the Lifestyle project

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WebsiteTemplate
+module NoTengoLifestyle
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "website_template_production"
+  # config.active_job.queue_name_prefix = "no_tengo_lifestyle"
 
   config.action_mailer.perform_caching = false
 
