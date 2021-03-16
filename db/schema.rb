ce0b4a3a1088efafa5f9449d2a4bee57fdd995e9
Oever González <software@notengobattery.com>
2021-03-16 13:25:36 -0600

    Add the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_16_191548) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["Name"], name: "index_users_on_Name", unique: true
+  end
+
 end
