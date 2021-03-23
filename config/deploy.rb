6edcdb9d97f069e6bd4f8b43e31d887d341d1600
Oever González <software@notengobattery.com>
2021-03-23 11:33:12 -0600

    Fix Mina's shared directory for ActiveStorage

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :user, "rails"
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs, fetch(:shared_dirs, []).push("tmp/pids", "tmp/sockets", "node_modules")
+set :shared_dirs, fetch(:shared_dirs, []).push("tmp/pids", "tmp/sockets", "node_modules", "storage")
 set :shared_files, fetch(:shared_files, []).push("config/master.key", "config/puma.rb", "config/secrets.yml")
 
 # This task is the environment that is loaded for all remote run commands, such as
