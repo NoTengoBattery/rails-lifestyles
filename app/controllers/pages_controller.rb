4abaf983553c55073c1e7ec35726f1df78987b66
Oever González <software@notengobattery.com>
2021-03-23 00:58:30 -0600

    Issolate seeds from Production

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :sign_in!, except: [:home]
 
   def home
-    @featured = Article.featured
+    @featured = Article.featured.class == Article ? Article.featured : Article.featured.first
     @categories = Category.top_categories
   end
 end
